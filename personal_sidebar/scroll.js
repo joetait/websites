0c8438ab3afbcc773614a5e4696e52d3f0190909
joetait <joetait@gmx.com>
2013-09-07 23:34:22 +0100

    added jquery to make navbar scroll horizontally, some stuff to todo

--- personal_sidebar/scroll.js
+++ personal_sidebar/scroll.js
@@ -1,4 +1,9 @@
-
-$(document).ready(function() {
-		alert("Hello world");
+/*this makes both contents of the navbar and the divider for the navbar scroll horizontally, whilst still being in fixed/absolute position*/
+$(window).scroll(function(){
+    $('#navcontainer').css('left','-'+$(window).scrollLeft()+'px');
+});
+$(window).scroll(function(){
+    var scroll = +$(window).scrollLeft();
+    var total = 160 - scroll;
+    $('#divider').css('left',total+'px');
 });
